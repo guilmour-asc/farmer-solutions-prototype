42b65e2fc1317589ec302d67d1b2f0030f5158e5
Guilmour Alves da Silva Cunha <guilmour.asc@gmail.com>
2017-10-21 10:55:07 -0200

    managed the feed page to gather data from JSON

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -15,6 +15,17 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class FeedPage {
 
+  public cardObject = {
+    cardAvatar: "assets/images/doggo.jpg",
+    cardUser: "Heracles",
+    cardDate: "November 5, 300 b.C.",
+    cardImage: "assets/images/autumn.jpg",
+    cardText: "Antaeus was supposed to fuck up my \"12 labors\" shit, bitching \'bout his invincibility and all... Well, fuck that, I knew he was Gaia's kiddo, so Antaeus and no ground would be my way to turn the table. He got lift, I smashed the shit outta him. I won. He lost. That fucking rooster...",
+    cardLikes: 12,
+    cardComments: 32,
+    cardTimePast: "11h ago"
+  }
+
   public username:string = "Heracles";
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
